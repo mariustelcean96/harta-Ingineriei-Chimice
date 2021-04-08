efde7211b9b142952dc9fae98653f753eac076ed
Marius-Andrei Telcean <marius.telcean96@gmail.com>
2021-04-08 20:01:02 +0300

    Add more style with CSS and JS

--- js/script.js
+++ js/script.js
@@ -1,3 +1,29 @@
+const MyHeading = document.querySelector('h1');
+MyHeading.textContent = "Primul mediu online integrat în care studenţii îşi pot spune părerea despre învăţământul universitar, iar acea părere contează!"
+
 function FuncţiaDeAur() {
     document.getElementById("demo7").innerHTML = "Continuă! Vei reuşi să realizezi ceea ce ţi-ai propus!";
+    }
+    /* Următoarea sintaxă e preluată din JavaScript Basics | MDN */
+let myButton = document.getElementById('b1');
+let myHeading = document.querySelector('h2');
+
+function setUserName() {
+let myName = prompt('Vă rog să vă introduceţi numele complet.');
+if(!myName){
+    setUserName();
+} else {
+localStorage.setItem('name', myName);
+myHeading.textContent = '#FăRaiDinCeAi, ' + myName;
 }
+}
+
+if(!localStorage.getItem('name')) {
+    setUserName();
+    } else {
+        let storedName = localStorage.getItem('name');
+        myHeading.textContent = '#FăRaiDinCeAi, ' + storedName;
+    }
+    myButton.onclick = function() {
+        setUserName();
+    }
