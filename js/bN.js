156d4f5b966704dcc4f1c9d1efc26ea54aba54ba
Telcean Marius-Andrei <marius.telcean96@gmail.com>
2022-04-28 15:10:45 +0300

    Clean up

---
+++ js/bN.js
@@ -0,0 +1,2 @@
+var markerTeraplast = L.marker([47.0608, 24.4223]).addTo(map);
+    markerTeraplast.bindPopup('Teraplast – ţevi şi profiluri de tâmplărie din PCV şi PP.');
---
+++ js/cj.js
@@ -0,0 +1,4 @@
+var markerEmersonCluj = L.marker([46.79373, 23.66319]).addTo(map);
+    markerEmersonCluj.bindPopup('Emerson Cluj – soluţii de automatizare şi control a proceselor. Oferă un bootcamp pe partea de simulări şi măsurători.' + '<br>' 
+    + '<a target="_blank" href="https://emerson.taleo.net/careersection/ex/jobsearch.ftl?">Pagina de cariere</a>' + '<br>' + 
+    '<a href="/imaginijob/programul-apprentice-emerson.jpg">Ucenicie la Emerson (studenţi în ultimul an)</a>');
