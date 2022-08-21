b63dfa6ec4685f5ec79e97bb297bae8714352902
Telcean Marius-Andrei <marius.telcean96@gmail.com>
2022-08-21 16:44:32 +0300

    Add Timiş district

---
+++ js/tm.js
@@ -0,0 +1,12 @@
+var markerPolitiaLugoj = L.marker([45.68514005498773, 21.904492140320773]).addTo(map);
+markerPolitiaLugoj.bindPopup(`
+
+<div class="comp-container">
+  <div class="comp-intro">
+    Poliţia locală Lugoj angajează absolvenţi în domeniu
+  </div>
+  <a href="imaginijob/politielugoj-compart-mediu.jpeg" target="_blank">Anunţ</a>
+  <a href="https://www.facebook.com/primariaLugoj/photos/a.1006754946196984/1637486809790458/" target="_blank">mai multe detalii</a>
+</div>
+
+`);
